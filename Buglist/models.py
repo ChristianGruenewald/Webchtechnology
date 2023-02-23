6dc9252fbf3dfba01750a25cbdc92fe90faaef23
Christian G <95079900+ChristianGruenewald@users.noreply.github.com>
2023-02-23 22:38:09 +0100

    init commit

---
+++ Buglist/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from datetime import date
+
+class Bugs(models.Model): 
+    kurzbeschreibung=models.CharField(max_length=40, unique=True)
+    beschreibung=models.CharField(max_length=200)
+    status=models.CharField(max_length=10) # Status ist codiert in: 'N'=neu, O=Offen, I= IN ARBEIT C=Geschlossen, P= BEREIT ZU PRÜFUNG
+    prio=models.CharField(max_length=10)
+    melder=models.CharField(max_length=40)
+    datum=models.DateField(default=date.today)
+
---
+++ Buglist/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from Buglist.views import index, EnterNewBug, addNewBug, ChangeBug, ProcessChangedBug, DeleteBug
+urlpatterns = [
+    path('index', index),
+    path('NewBug/',EnterNewBug),
+    path('addNewBug/',addNewBug), 
+    path('ChangeBug',ChangeBug), 
+    path('ProcessChangeBug', ProcessChangedBug),
+    path('Deletebug', DeleteBug),
+]
